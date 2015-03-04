0f7fa8de60e3024d9172f92ae7acc034c25b79d2
Sriharish <sriharish.admin@pe.tamu.edu>
2015-03-04 00:55:32 -0600

    Added forked links content script

---
+++ Frontier/Frontier/app/forked_links.js
@@ -0,0 +1,19 @@
+﻿$(document).ready((function (){
+   
+    "use strict";
+    
+    var $list = $("#list_container .url_list");
+    var tmplt = "<li><h3>{{url}}</h3></li>";
+    
+    
+    
+    function updateList ( urls ) {
+        $list.html($.map( urls, function ( url ) {
+            return tmplt.replace( /{{url}}/, url );
+        }).join(""));
+    }
+    
+    updateList( ["yahoo.com", "Richard", "Kendra", "Jake"]);
+ 
+    
+}()));
