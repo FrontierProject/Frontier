11de743e4d6b5d2170f1a3f2b3e43747d2e81f4b
Sriharish <sriharish.admin@pe.tamu.edu>
2015-03-06 03:43:09 -0600

    Working forked link menu

--- Frontier/Frontier/app/forked_links.js
+++ Frontier/Frontier/app/forked_links.js
@@ -1,19 +1,58 @@
-﻿$(document).ready((function (){
+﻿//<![CDATA[ 
+$(window).load(function(){
+$(document).ready((function (){
    
-    "use strict";
     
-    var $list = $("#list_container .url_list");
-    var tmplt = "<li><h3>{{url}}</h3></li>";
+        //$ indicates selector
+        var $list = $("#list_container #url_list");
+        var $listItems = $("#list_container #url_list .list_item");
+        var tmplt = "<li class=\"list_item\"><p>{{url}}</p></li>";
     
+        //dynamically populates the list and animates
+        function updateList ( urls ) {
+            $list.html($.map( urls, function ( url ) {
+                return tmplt.replace( /{{url}}/, url );
+            }).join(""));
+        }
+        function showList(){
+            //showList
+            $list.fadeIn(1000);
+            $("li").animate({
+                'border-width' : '+=1px',
+                'padding' : '+=10px'
+            });
+
+            //hideList after extending
+            $("li").animate({
+                'margin-right' : '-=250px'
+            }, 500).delay( 5000 );   
+            
+            $("li").fadeOut(500);
+            
+            
+        }
+        function highlightOption(){
+            $("li").mouseenter(
+                function(){
+                    $(this).css({
+                        'margin-right' : '5px'
+                    });
+                }
+            );
+            $("li").mouseleave(
+                function(){
+                    $(this).css({
+                        'margin-right' : '10px'
+                    });
+                }
+            );
+        }
     
+        //fades the list into view
+        $list.hide(500);
+        updateList(["thing 1", "thing 2", "thing 3", "thing 4"]);
+        showList();
+        highlightOption();
     
-    function updateList ( urls ) {
-        $list.html($.map( urls, function ( url ) {
-            return tmplt.replace( /{{url}}/, url );
-        }).join(""));
-    }
-    
-    updateList( ["yahoo.com", "Richard", "Kendra", "Jake"]);
- 
-    
-}()));
+    }()));
+});//]]>
