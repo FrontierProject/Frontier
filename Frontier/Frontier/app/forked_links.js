5a3fd2e2210a23d3bf90294e64c5bcd310419852
Joseph Newman <jwnewman@tamu.edu>
2015-04-02 13:33:09 -0500

    Moving from jQuery styling to CSS styling.  Forward/back links both functional.

--- Frontier/Frontier/app/forked_links.js
+++ Frontier/Frontier/app/forked_links.js
@@ -5,64 +5,65 @@ $(document).ready((function (){
     //Tested code here: http://jsfiddle.net/mvCUH/198/
     
         //$ indicates selector
-        var $list = $("#list_container #url_list");
-        var $listItems = $("#list_container #url_list .list_item");
+        // var $forwardList = $("#forward_list_container #url_list");
+        // var $backList = $("#forward_list_container #url_list");
+        // var $listItems = $("#forward_list_container #back_list_contrainer #url_list .list_item");
         var tmplt = "<li class=\"list_item\"><p>{{url}}</p></li>";
         
-        var imports = "<link rel=\"stylesheet\" type=\"text/css\" href=\"forked_links.css\" /><script src=\"jquery-2.1.3.min.js\"></script><script src=\"forked_links.js\"></script>";
+        // var imports = "<link rel=\"stylesheet\" type=\"text/css\" href=\"forked_links.css\" /><script src=\"jquery-2.1.3.min.js\"></script><script src=\"forked_links.js\"></script>";
         var forwardData = "";
         var backData = "";
 
         //functions
         //dynamically populates the list and animates
         function updateList(backLinks, forwardLinks) {
-            $("head").append(imports);
+            // $("head").append(imports);
             
-            //var backMenu = $.map(backLinks, function (backLink) {
-            //    return tmplt.replace(/{{url}}/, backLink.url);
-            //}).join("");
-
-            //backData = "<div id=\"list_container\"><ul style=\"list-style-type:none;\" id=\"url_list\">" + backMenu + "</ul></div>";
-            //$("body").append(backData);
-
-            var forwardMenu = $.map(forwardLinks, function (forwardLink) {
-                return tmplt.replace(/{{url}}/, forwardLink[0]);
+            var backMenu = $.map(backLinks, function (backLink) {
+               return tmplt.replace(/{{url}}/, backLink);
             }).join("");
 
-            forwardData = "<div id=\"list_container\"><ul style=\"list-style-type:none;\" id=\"url_list\">" + forwardMenu + "</ul></div>";
+            backData = "<div id=\"back_list_container\"><ul style=\"list-style-type:none;\" id=\"back_url_list\">" + backMenu + "</ul></div>";
+            $("body").append(backData);
+
+            var forwardMenu = $.map(forwardLinks, function (forwardLink) {
+                return tmplt.replace(/{{url}}/, forwardLink);
+            }).join("");
+
+            forwardData = "<div id=\"forward_list_container\"><ul style=\"list-style-type:none;\" id=\"forward_url_list\">" + forwardMenu + "</ul></div>";
             $("body").append(forwardData);
         }
-        function showList() {
-            //showList
-            $list.fadeIn(1000);
-            //hideList after extending
-            $(".list_item").animate({
-                'margin-right': '-=250px'
-            }, 800).delay(5000);
-            //$(".list_item").fadeOut(500);
-        }
-        function highlightOption() {
-            $(".list_item").mouseenter(
-                function () {
-                    $(this).css({
-                        'margin-right': '5px'
-                    });
-                }
-            );
-            $(".list_item").mouseleave(
-                function () {
-                    $(this).css({
-                        'margin-right': '10px'
-                    });
-                }
-            );
-        }
+        // function showList() {
+            // //showList
+            // $list.fadeIn(1000);
+            // //hideList after extending
+            // $(".list_item").animate({
+                // 'margin-right': '-=250px'
+            // }, 800).delay(5000);
+            // //$(".list_item").fadeOut(500);
+        // }
+        // function highlightOption() {
+            // $(".list_item").mouseenter(
+                // function () {
+                    // $(this).css({
+                        // 'margin-right': '5px'
+                    // });
+                // }
+            // );
+            // $(".list_item").mouseleave(
+                // function () {
+                    // $(this).css({
+                        // 'margin-right': '10px'
+                    // });
+                // }
+            // );
+        // }
 
         
 
 
         //fades the list into view
-        $list.hide(1000);
+       // $list.hide(1000);
         
         chrome.runtime.sendMessage({
             type: "FORKED_LINKS",
@@ -71,8 +72,8 @@ $(document).ready((function (){
             updateList(response.backLinks, response.forwardLinks);
         });
 
-        showList();
-        highlightOption();
+        // showList();
+        // highlightOption();
 
     }()));
 });//]]>
