a71c2a9b7326524b80bc3c4927d3fa45a4ec6ab4
Joseph Newman <jwnewman@tamu.edu>
2015-04-20 00:20:50 -0500

    - Dropdown menu now only spans right portion of top of page - Added Frontier logo and "Frontier" text - Borders now between each link and the dropdown itself

--- Frontier/Frontier/app/page_ui.js
+++ Frontier/Frontier/app/page_ui.js
@@ -12,41 +12,144 @@
 
         //functions
         //dynamically populates the list and animates
-        function updateList() {
+        // function updateList() {
             
-            // Only display when there is a fork
-            if (backLinks.length > 1)
-            {
-                var backContainer = d3.select("body").append("div")
-                    .attr("id", "back_list_container")
-                    .attr("class", "yui3-cssreset");
+            // // Only display when there is a fork
+            // if (backLinks.length > 1)
+            // {
+                // var backContainer = d3.select("body").append("div")
+                    // .attr("id", "back_list_container")
+                    // .attr("class", "yui3-cssreset");
                 
-                var backList = backContainer.append("ul")
-                    .attr("id", "back_url_list")
-                    .selectAll("li")
-                    .data(backLinks)
-                    .enter()
-                    .append("li")
-                    .append("p")
-                    .text(function(link) { return link; });
-            }
+                // var backList = backContainer.append("ul")
+                    // .attr("id", "back_url_list")
+                    // .selectAll("li")
+                    // .data(backLinks)
+                    // .enter()
+                    // .append("li")
+                    // .append("p")
+                    // .text(function(link) { return link; });
+            // }
             
-            // Only display when there is a fork
-            if (forwardLinks.length > 1)
-            {
-                var forwardContainer = d3.select("body").append("div")
-                    .attr("id", "forward_list_container")
-                    .attr("class", "yui3-cssreset");
+            // // Only display when there is a fork
+            // if (forwardLinks.length > 1)
+            // {
+                // var forwardContainer = d3.select("body").append("div")
+                    // .attr("id", "forward_list_container")
+                    // .attr("class", "yui3-cssreset");
                 
-                var forwardList = forwardContainer.append("ul")
-                    .attr("id", "forward_url_list")
-                    .selectAll("li")
-                    .data(forwardLinks)
-                    .enter()
-                    .append("li")
-                    .append("p")
-                    .text(function(link) { return link; });
-            }
+                // var forwardList = forwardContainer.append("ul")
+                    // .attr("id", "forward_url_list")
+                    // .selectAll("li")
+                    // .data(forwardLinks)
+                    // .enter()
+                    // .append("li")
+                    // .append("p")
+                    // .text(function(link) { return link; });
+            // }
+        // }
+        
+        function rightBanner()
+        {
+             // Appends the div
+            var bannerContainer = d3.select("body").append("div")
+                .attr("id", "frontier_banner")
+                .attr("class", "yui3-cssreset");
+            
+            // The link icons are in an unordered list
+            var bannerLinks = bannerContainer.append("ul")
+                .attr("id", "frontier_banner_list");
+            
+            // Adds the close button to the top right of the banner
+            var closeButton = bannerLinks.append("li")
+                .attr("id", "frontier_close_button")
+                
+                .append("img")
+                .attr("src", chrome.extension.getURL("img/close_icon_16.png"));
+            
+            $("#frontier_close_button").click(function () {
+                hideBanner = setTimeout(function() {
+                    $("#frontier_banner").slideUp();
+                }, 0);
+            });
+            
+            // Add the history button
+            var historyButton = bannerLinks.append("li")
+                .attr("id", "frontier_history_button");
+            
+            historyButton.append("img")
+                .attr("src", chrome.extension.getURL("img/history_icon_16.png"));
+            
+            $("#frontier_history_button").click(function () {
+                chrome.runtime.sendMessage({
+                    type: "OPEN_HISTORY"
+                });
+            });       
+            
+            // Add the forward link image
+            var forwardLinkButton = bannerLinks.append("li")
+                .attr("id", "frontier_forwardlinks")
+            
+            forwardLinkButton.append("img")
+                .attr("src", chrome.extension.getURL("img/forward_icon_16.png"));
+            
+            // Add all of the forward links
+            forwardLinkButton.append("ul")
+                .selectAll("li")
+                .data(forwardLinks)
+                .enter()
+                .append("li")
+                .append("a")
+                .attr("href", function(link) { 
+                    if (window.location.protocol == "https:")
+                        return "https://" + link;
+                    else
+                        return "http://" + link;
+                })
+                .attr("target", "_blank")
+                .text(function(link) { return link; });
+            
+            // Add the back link image
+            var backLinkButton = bannerLinks.append("li")
+                .attr("id", "frontier_backlinks")
+                
+            backLinkButton.append("img")
+                .attr("src", chrome.extension.getURL("img/back_icon_16.png"));
+            
+            // Add all of the back links
+            backLinkButton.append("ul")
+                .selectAll("li")
+                .data(backLinks)
+                .enter()
+                .append("li")
+                .append("a")
+                .attr("href", function(link) { 
+                    if (window.location.protocol == "https:")
+                        return "https://" + link;
+                    else
+                        return "http://" + link;
+                })
+                .attr("target", "_blank")
+                .text(function(link) { return link; });
+            
+            bannerContainer.append("span")
+                .text("FRONTIER");
+            
+            bannerContainer.append("img")
+                .style("float", "right")
+                .attr("src", chrome.extension.getURL("img/frontier_icon_16.png"));
+            
+            // Keep the banner showing while the mouse is in it
+            $("#frontier_banner").mouseenter( function(event) {
+                clearTimeout(hideBanner);
+            });
+            
+            // When mouse leaves, hide it after 2 seconds
+            $("#frontier_banner").mouseleave( function(event) {
+                hideBanner = setTimeout(function() {
+                    $("#frontier_banner").slideUp();
+                }, 5000);
+            });
         }
         
         // Injects banner into current page
@@ -141,7 +244,7 @@
             $("#frontier_banner").mouseleave( function(event) {
                 hideBanner = setTimeout(function() {
                     $("#frontier_banner").slideUp();
-                }, 2000);
+                }, 5000);
             });
         }
         
@@ -152,7 +255,8 @@
             backLinks = response.backLinks;
             forwardLinks = response.forwardLinks;
             // updateList();
-            addBanner();
+            // addBanner();
+            rightBanner();
         });
         
         var hideBanner;
@@ -164,7 +268,7 @@
                 clearTimeout(hideBanner);
                 hideBanner = setTimeout(function() {
                     $("#frontier_banner").slideUp();
-                }, 2000);
+                }, 5000);
             });
         }
         
